4b4012570ab6b85eb18b0f0112ca4ba459e87ea6
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-09-19 11:32:08 +0300

    - Added http package. - handle permission internet in Android mainfast. - Added Home Screen. - Added Articals model.

---
+++ lib/features/home/models/article_news_model.dart
@@ -0,0 +1,43 @@
+class ArticleNewsModel {
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  ArticleNewsModel({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
+  }
+
+  factory ArticleNewsModel.fromJson(Map<String, dynamic> map) {
+    return ArticleNewsModel(
+      author: map['author'] ?? "",
+      title: map['title'],
+      description: map['description'],
+      url: map['url'],
+      urlToImage: map['urlToImage'],
+      publishedAt: map['publishedAt'],
+      content: map['content'],
+    );
+  }
+}
