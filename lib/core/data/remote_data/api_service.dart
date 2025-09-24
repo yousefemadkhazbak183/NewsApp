2ffcd125e16f9d482b365e6459b29f051673e181
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-09-24 17:32:51 +0300

    - Added Remote data source file. ✅ - Added Local data source file. ✅ - Added api confige for API's constant. ✅ - Added Api services file. ✅

---
+++ lib/core/data/remote_data/api_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:flutter_news_app/core/data/remote_data/api_config.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<dynamic> get(String endPoint, {Map<String, dynamic>? query}) async {
+    final url = Uri.http(ApiConfig.baseUrl, 'v2/$endPoint', {
+      "apiKey": ApiConfig.apiKey,
+      ...?query,
+    });
+
+    final http.Response response = await http.get(url);
+
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
+}
