d19ae744787c98c737a05c38a66b82a31690dc4f
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-11-13 19:06:43 +0200

    - Make shared component for view all widget. ✅

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/features/home/components/view_all_component.dart';
 import 'package:flutter_news_app/features/home/components/trending_news.dart';
 
 import 'package:flutter_news_app/features/home/controller/home_controller.dart';
@@ -14,7 +15,14 @@ class HomeScreen extends StatelessWidget {
       create: (BuildContext context) => HomeController(),
       child: Consumer<HomeController>(
         builder: (BuildContext context, controller, Widget? child) {
-          return const Scaffold(body: Column(children: [TrendingNews()]));
+          return Scaffold(
+            body: Column(
+              children: [
+                const TrendingNews(),
+                ViewAllComponent(title: "Categories", titleColor: const Color(0xFF141414), onTap: () {}),
+              ],
+            ),
+          );
         },
       ),
     );
